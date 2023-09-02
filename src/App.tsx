65be4bc7613ed0e3cfd76aaac90d6313041ede97
= <ganna.andrushko@gmail.com>
2023-09-02 11:49:52 +0800

    clerk authentication added

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from './pages/Layout';
 
 import Home from './pages/home/Home';
+import ClerkProviderWithRoutes from './provider/ClerkProvider';
 
 
 function App() {
   return (
    
     <BrowserRouter>
-       <Routes>
+    <ClerkProviderWithRoutes />
+        {/* <Routes>
          <Route path="/" element={<Layout />}>
            <Route index element={<Home />} />
 
-          {/*<Route path="blogs" element={<Blogs />} />
+          <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />  */}
+          <Route path="*" element={<NoPage />} />  
         </Route>
-      </Routes>
+      </Routes>  */}
     </BrowserRouter>
   );
 }
---
+++ src/ProtectedPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type ProtectedPageProps = {
+    
+};
+
+const ProtectedPage:React.FC<ProtectedPageProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default ProtectedPage;
